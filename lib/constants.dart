61bc85f1d516ea5abf971e27815d2fcb1266e393
Annurdien Rasyid <ajwadax@gmail.com>
2022-06-14 11:11:40 +0700

    chore: add constant

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+class Constants {
+  static initVersion() async {}
+
+  static String SUPABASE_URL = "";
+  static String SUPABASE_ANON_KEY =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0bHZjb21qYWFhemp2eXpjY2F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQ2MDI1NDgsImV4cCI6MTk3MDE3ODU0OH0.UwoC5Vffyx9UYZxI16qCRPPbg3MRUh4st3hWVb_47v8";
+  static bool isDebugMode = kDebugMode;
+  static String BOX_NAME = "life_app";
+}
