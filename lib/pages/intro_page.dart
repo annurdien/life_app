c978afe35b2f3c09276b1a47665f638800a04e8b
Annurdien Rasyid <ajwadax@gmail.com>
2022-06-14 11:14:33 +0700

    feat: add intro and my home page

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("INTROOO"),
+      ),
+    );
+  }
+}
---
+++ lib/pages/my_home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("ASWW CENTER"),
+      ),
+    );
+  }
+}
