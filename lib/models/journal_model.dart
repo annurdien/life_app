fbcf6f056d35bc085390c99104dbffe8ba23384e
Annurdien Rasyid <ajwadax@gmail.com>
2022-06-14 11:12:49 +0700

    feat: add journal model

---
+++ lib/models/journal_model.dart
@@ -0,0 +1,32 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'journal_model.freezed.dart';
+part 'journal_model.g.dart';
+
+@freezed
+class JournalModel with _$JournalModel {
+  const factory JournalModel({
+    required String title,
+    required String body,
+    required int feeling,
+    required List<String> emotion,
+    required DateTime created_at,
+  }) = _JournalModel;
+
+  factory JournalModel.fromJson(Map<String, dynamic> json) =>
+      _$JournalModelFromJson(json);
+}
+
+@freezed
+class JournalDto with _$JournalDto {
+  const factory JournalDto({
+    required String title,
+    required String body,
+    required int feeling,
+    required List<String> emotion,
+    required DateTime created_at,
+  }) = _JournalDto;
+
+  factory JournalDto.fromJson(Map<String, dynamic> json) =>
+      _$JournalDtoFromJson(json);
+}
