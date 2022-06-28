99c007c166f6d825c31c4e586e9898073b899fa1
Annurdien Rasyid <ajwadax@gmail.com>
2022-06-29 01:05:01 +0700

    feat: add relief page

--- lib/pages/relief_page.dart
+++ lib/pages/relief_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:life_app/themes/custom_theme.dart';
+
+import '../core/provider/music_provider.dart';
+import '../utils.dart';
+import '../widgets/music_card.dart';
 
 class ReliefPage extends StatefulHookConsumerWidget {
   const ReliefPage({Key? key}) : super(key: key);
@@ -11,9 +17,47 @@ class ReliefPage extends StatefulHookConsumerWidget {
 class _ReliefPageState extends ConsumerState<ReliefPage> {
   @override
   Widget build(BuildContext context) {
+    final textTheme = context.textTheme;
+    final colors = context.colors;
+
+    final musics = ref.watch(musicsProvider);
+
     return Scaffold(
-      body: Center(
-        child: Text("Relief"),
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20.w,
+            vertical: 15.h,
+          ),
+          children: [
+            Text(
+              "Relief",
+              style: textTheme.title.withColor(colors.snow),
+              textAlign: TextAlign.center,
+            ),
+            30.verticalSpace,
+            musics.when(
+              data: (data) => Wrap(
+                spacing: 30.w,
+                runSpacing: 10.h,
+                children: [
+                  for (final relief in data) ...[
+                    MusicCard(relief: relief),
+                  ],
+                ],
+              ),
+              error: (_, __) {
+                logger.i(__);
+                return const Center(
+                  child: Text("Error"),
+                );
+              },
+              loading: () => const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
