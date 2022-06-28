66bfeea8c66fd08881fef256a094d31484260e85
Annurdien Rasyid <ajwadax@gmail.com>
2022-06-29 01:04:43 +0700

    feat: add my home page

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,13 +1,15 @@
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:life_app/themes/custom_theme.dart';
 
 import 'home_page.dart';
 import 'profile_page.dart';
 import 'relief_page.dart';
+
+final pageNumberProvider = StateProvider<int>((ref) => 0);
 
 class MyHomePage extends StatefulHookConsumerWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -19,25 +21,36 @@ class MyHomePage extends StatefulHookConsumerWidget {
 class _MyHomePageState extends ConsumerState<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    final pagecontroller = usePageController(
+    final activeIndex = useState<int>(0);
+
+    final pageController = usePageController(
       initialPage: 0,
     );
 
-    final activeIndex = useState<int>(0);
+    final bottomNavBarConttoller = useTabController(initialLength: 3);
 
     final colors = context.colors;
-    final textTheme = context.textTheme;
+
+    ref.listen<int>(pageNumberProvider, (prev, next) {
+      bottomNavBarConttoller.animateTo(next);
+      pageController.jumpToPage(next);
+    });
 
     return Scaffold(
       body: PageView(
-        controller: pagecontroller,
+        controller: pageController,
         children: const [
           HomePage(),
           ReliefPage(),
           ProfilePage(),
         ],
+        onPageChanged: (index) {
+          bottomNavBarConttoller.animateTo(index);
+          ref.read(pageNumberProvider.notifier).update((state) => index);
+        },
       ),
       bottomNavigationBar: ConvexAppBar(
+        controller: bottomNavBarConttoller,
         initialActiveIndex: activeIndex.value,
         backgroundColor: const Color(0xFF2F4767),
         color: colors.snow.withOpacity(0.5),
@@ -60,8 +73,9 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
           ),
         ],
         onTap: (index) {
-          pagecontroller.jumpToPage(index);
+          pageController.jumpToPage(index);
           activeIndex.value = index;
+          ref.read(pageNumberProvider.notifier).update((state) => index);
         },
       ),
     );
