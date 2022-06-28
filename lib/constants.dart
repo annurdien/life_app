40350fb33e4483d17139101a31990130ff7aaa30
Annurdien Rasyid <ajwadax@gmail.com>
2022-06-29 01:02:39 +0700

    chore: update constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,4 +8,7 @@ class Constants {
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0bHZjb21qYWFhemp2eXpjY2F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQ2MDI1NDgsImV4cCI6MTk3MDE3ODU0OH0.UwoC5Vffyx9UYZxI16qCRPPbg3MRUh4st3hWVb_47v8";
   static bool isDebugMode = kDebugMode;
   static String BOX_NAME = "life_app";
+
+  static String IMAGE_PLACEHOLDER =
+      "https://cdn.discordapp.com/attachments/900022715321311258/933381528157831260/259219153_1761134020752132_5367289037432796973_n.jpg";
 }
