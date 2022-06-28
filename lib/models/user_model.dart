36055550e3e8ce9d74dcc3ce205935aaa10997ee
Annurdien Rasyid <ajwadax@gmail.com>
2022-06-29 01:04:08 +0700

    feat: add user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,20 +1,64 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hive/hive.dart';
 
 part 'user_model.freezed.dart';
 part 'user_model.g.dart';
 
-@freezed
-class UserModel with _$UserModel {
-  const factory UserModel({
-    required String? id,
-    required String email,
-    required String image,
-    required String name,
-    required String? phone,
-  }) = _UserModel;
+@HiveType(typeId: 0)
+class UserModel extends HiveObject {
+  UserModel({
+    this.id,
+    required this.name,
+    required this.email,
+    this.phone,
+    this.image,
+  });
 
-  factory UserModel.fromJson(Map<String, dynamic> json) =>
-      _$UserModelFromJson(json);
+  @HiveField(0)
+  late final String? id;
+  @HiveField(1)
+  late final String name;
+  @HiveField(2)
+  late final String email;
+  @HiveField(3)
+  late final String? phone;
+  @HiveField(4)
+  late final String? image;
+
+  UserModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    phone = json['phone'];
+    image = json['image'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['email'] = email;
+    data['phone'] = phone;
+    data['image'] = image;
+
+    return data;
+  }
+
+  UserModel copyWith({
+    String? id,
+    String? name,
+    String? email,
+    String? phone,
+    String? image,
+  }) {
+    return UserModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      phone: phone ?? this.phone,
+      image: image ?? this.image,
+    );
+  }
 }
 
 @freezed
